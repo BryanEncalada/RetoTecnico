8dfbcca5ee1fabe56eec2078cf60ece2266e5403
Bryan <bryanencalada@gmail.com>
2024-10-31 16:52:36 -0500

    8ok

--- src/config/database.js
+++ src/config/database.js
@@ -55,10 +55,22 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+   // port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
   }
 );
+
+
+// const sequelize = new Sequelize(
+//   "pettimep_pettimeBD",
+//   "pettimep_pettimeBD",
+//   "Pettime@123",
+//   {
+//     host: "201.148.107.87",
+//     dialect: "mysql",
+//   }
+// );
+
 
 /**
  * @function dbConnection
